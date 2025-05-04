8354eac18fc0d6a2e7554b309294c50e030fc7a5
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-04 14:23:34 -0500

    Additional options carries over from previous config.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,29 @@
 -- |__| |     |  | |__| | \| ___]
 --
 -- #########################################################################
+
+vim.cmd("let g:netrw_liststyle = 3")
+
 local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+-- Turn off swapfile
+opt.swapfile = false
+
+-- Turn off italic comments
+vim.api.nvim_set_hl(0, "Comment", { italic = false })
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- Clipboard
+opt.clipboard:append("unnamedplus") -- use the system clipboard as the default register:
