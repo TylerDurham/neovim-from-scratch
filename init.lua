2ff0f1170b05b462456b5ab4e3fbf5ee9ca149a1
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-04 13:15:45 -0500

    Moved lazy to first.

--- init.lua
+++ init.lua
@@ -5,8 +5,6 @@
 --
 -- #########################################################################
 
-require("config.keymaps")
-
-require("config.options")
-
 require("config.lazy")
+require("config.keymaps")
+require("config.options")
